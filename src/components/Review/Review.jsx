980f712d652469e1aa8b40b896cb3f44f10287e0
miyadhossain <miyad.mh@gmail.com>
2021-05-29 14:09:02 +0600

    review page design update

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import { getSpecificPropertyInfo } from "../../app/actions/dataAction";
+import "./Review.css";
 
 const Review = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleGetSpecificPropertyData = (id) => {
+    history.push(`/checkout/${id}`);
+  };
 
   useEffect(() => {
     dispatch(getSpecificPropertyInfo(parseInt(id, 10)));
-  }, [id]);
+  }, []);
 
   const { data } = useSelector((state) => ({
     data: state.dataReducer.specificPropertyInfo,
@@ -17,7 +23,82 @@ const Review = () => {
 
   return (
     <div>
-      <h1>Tanvin</h1>
+      <div
+        style={{ backgroundImage: `url(${data.homeImg})` }}
+        className="main__Image"
+      >
+        <div className="main__text">
+          <h2>Price : BDT {data.price}</h2>
+          <div className="buttonGroup">
+            <button
+              onClick={() => handleGetSpecificPropertyData(data.id)}
+              className="_btn book__btn"
+            >
+              Book Now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container mt-5">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-6">
+            <img
+              style={{ width: "500px", height: "250px", borderRadius: "10px" }}
+              className="img-fluid"
+              src={data.bedroomImg}
+              alt=""
+            />
+          </div>
+          <div className="col-md-6">
+            <h3>Bed Room</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              necessitatibus consequatur deleniti neque incidunt labore
+              laudantium optio nesciunt atque eveniet!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="container mt-5">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-6">
+            <h3>Drawing Room</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              necessitatibus consequatur deleniti neque incidunt labore
+              laudantium optio nesciunt atque eveniet!
+            </p>
+          </div>
+          <div className="col-md-6">
+            <img
+              style={{ width: "500px", height: "250px", borderRadius: "10px" }}
+              className="img-fluid"
+              src={data.drawingRoomImg}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container mt-5">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-6">
+            <img
+              style={{ width: "500px", height: "250px", borderRadius: "10px" }}
+              className="img-fluid"
+              src={data.kitchenImg}
+              alt=""
+            />
+          </div>
+          <div className="col-md-6">
+            <h3>Kitchen Room</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              necessitatibus consequatur deleniti neque incidunt labore
+              laudantium optio nesciunt atque eveniet!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
