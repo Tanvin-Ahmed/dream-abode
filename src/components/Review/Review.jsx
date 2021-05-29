eb33629e45ee5fd87b3e781b7135b06b64592b7a
miyadhossain <miyad.mh@gmail.com>
2021-05-29 10:22:36 +0600

    review page modify

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -5,6 +5,7 @@ const Review = () => {
     <div>
       <h1>This is Review Page</h1>
       <h4>This is another changed from review page branch</h4>
+      <h6>This is merged from review page</h6>
     </div>
   );
 };
